675420bddde7960502d1811eed83ae6177215e12
vishnuvarthan23200229-web <vishnuvarthang2024@gmail.com>
2026-03-08 21:52:34 +0900

    Initial commit: Data Structure and Algorithms in Java

---
+++ Stack_Implementation/stack_implementation.java
@@ -0,0 +1,56 @@
+class stack<T>{
+    private T [] arr;
+    private int top;
+    public stack(int  size){
+        arr = (T[])new Object[size];
+        top  = -1;
+    }
+    public boolean isFull(){
+        return top == arr.length-1;
+    }
+    public void push(T val){
+        if(isFull()){
+            return;
+        }
+        top++;
+        arr[top] = val;
+    }
+    public boolean isEmpty(){
+        return top == -1;
+    }
+    public T pop(){
+        if(isEmpty()){
+            return null;
+        }
+       return arr[top--];
+    }
+    public void display(){
+        for(int i = top;i>=0;i--){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public T peek(){
+        if(isEmpty()){
+            return null;
+        }
+        return arr[top];
+    }
+    public int size(){
+        if(isEmpty()){
+            return -1;
+        }
+        return top+1;
+    }
+}
+public class stack_implementation
+{
+	public static void main(String[] args) {
+		stack<String> st = new stack(5); // using generics
+		st.push("vishnu");
+		st.push("mahe");
+		st.push("deepak");
+		System.out.print(st.peek());
+		System.out.println();
+		st.display();
+	}
+}
